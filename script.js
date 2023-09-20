dd969f273134ddf78bbd243056e8e2f932bdb4ef
Ksoler09 <145513223+Ksoler09@users.noreply.github.com>
2023-09-19 19:44:26 -0500

    inicio

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('boton1').addEventListener('click', function() {
+    alert('Haz hecho clic en el Botón 1');
+});
+
+document.getElementById('boton2').addEventListener('click', function() {
+    alert('Haz hecho clic en el Botón 2');
+});
