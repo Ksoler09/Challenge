fedcf0b481824f93f5e5c280b8c8720df2f17cac
Ksoler09 <karen.solern@hotmail.com>
2023-09-19 20:50:40 -0500

    PET GET OK

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document.getElementById('boton1').addEventListener('click', function() {
-    alert('Haz hecho clic en el Botón 1');
+document.getElementById('boton1').addEventListener('click', async function() {
+    const sizeValue = document.getElementById('texto').value;
+    if (!sizeValue || isNaN(sizeValue)) {
+        alert('Por favor, introduce un número válido en el cuadro de texto.');
+        return;
+    }
+    
+    const apiUrl = "https://8j5baasof2.execute-api.us-west-2.amazonaws.com/production/tests/trucode/samples?size=" + sizeValue;
+    
+    try {
+        const response = await fetch(apiUrl);
+        
+        const data = await response.text();
+        alert(data);
+    } catch (error) {
+        alert('Error al realizar la petición: ' + error.message);
+    }
 });
 
 document.getElementById('boton2').addEventListener('click', function() {
