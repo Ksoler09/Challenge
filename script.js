bd239a0ac46d37f2f0ba8f5888b7dfa0111f3d7b
Ksoler09 <karen.solern@hotmail.com>
2023-09-19 21:35:38 -0500

    CORREC LIMIT REGISTROS Y AJUSTE FORMATO JSON

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 document.getElementById('boton1').addEventListener('click', async function() {
-    const sizeValue = document.getElementById('texto').value;
+    const sizeValue = parseInt(document.getElementById('texto').value, 10);
+    
     if (isNaN(sizeValue) || sizeValue < 1 || sizeValue > 100) {
         alert('Por favor, introduce un número entero válido entre 1 y 100 en el cuadro de texto.');
         return;
     }
     
-    const apiUrl = "https://8j5baasof2.execute-api.us-west-2.amazonaws.com/production/tests/trucode/samples?size=" + sizeValue;
+    const apiUrl = "https://8j5baasof2.execute-api.us-west-2.amazonaws.com/production/tests/trucode/samples?size=" + (sizeValue-1);
     
     try {
         const response = await fetch(apiUrl);
         
+   
         const data = await response.text();
-        alert(data);
+        
+        
+        const lines = data.split('\n');
+        const formattedData = lines.map(line => {
+            const [name, phone, email] = line.split(',');
+            return { name, email, phone };
+        });
+
+        console.log(formattedData);
+
+        alert(JSON.stringify(formattedData, null, 2));
     } catch (error) {
         alert('Error al realizar la petición: ' + error.message);
     }
